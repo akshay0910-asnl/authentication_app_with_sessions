8b51f0a9bf23b28cbfc4bb9531386952eb07ea66
Akshay Kumar <akgunner0910@gmail.com>
2024-01-05 14:20:31 +0530

    Changing connection string

--- connections/db.js
+++ connections/db.js
@@ -2,25 +2,15 @@
 
 const { Pool } = require('pg');
 const Knex = require('knex');
-const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT } = process.env;
+const { DB_CONNECTION_STRING } = process.env;
 
 const pool = new Pool({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_DATABASE,
-    password: DB_PASSWORD,
-    port: parseInt(DB_PORT),
+    connectionString: DB_CONNECTION_STRING,
 });
 
 const knex = Knex({
     client: 'pg',
-    connection: {
-        user: DB_USER,
-        host: DB_HOST,
-        database: DB_DATABASE,
-        password: DB_PASSWORD,
-        port: parseInt(DB_PORT),
-    },
+    connection: DB_CONNECTION_STRING,
 });
 
 module.exports = {pool, knex};
